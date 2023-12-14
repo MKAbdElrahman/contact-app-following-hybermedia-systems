ab8c46d7ec37c2691d21d51e9b189bb938b8f864
MKAbdElrahman <mkcodedev@gmail.com>
2023-12-14 03:56:51 +0200

    r:extract the db seeding out of main

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"app/db"
-	"app/domain"
 	"app/handler"
 	"app/service"
 	"context"
@@ -58,22 +57,8 @@ func run(ctx context.Context, logger *slog.Logger) error {
 	}
 
 	contactStore := db.NewInMemoryContactStore()
-	contacts := []domain.Contact{
-		{FirstName: "Mohamed", LastName: "Ali", Email: "mohamed.ali@example.com", Phone: "+211111111111"},
-		{FirstName: "Sayed", LastName: "Kamal", Email: "sayed.kamal@example.com", Phone: "+511111111111"},
-		{FirstName: "John", LastName: "Doe", Email: "john.doe@example.com", Phone: "+1234567890"},
-		{FirstName: "Jane", LastName: "Doe", Email: "jane.doe@example.com", Phone: "+9876543210"},
-		{FirstName: "Alice", LastName: "Smith", Email: "alice.smith@example.com", Phone: "+1122334455"},
-		{FirstName: "Bob", LastName: "Johnson", Email: "bob.johnson@example.com", Phone: "+9988776655"},
-		{FirstName: "Emily", LastName: "Wilson", Email: "emily.wilson@example.com", Phone: "+6677889900"},
-		{FirstName: "David", LastName: "Miller", Email: "david.miller@example.com", Phone: "+5544332211"},
-		{FirstName: "Sophia", LastName: "Clark", Email: "sophia.clark@example.com", Phone: "+6677001122"},
-		{FirstName: "Daniel", LastName: "Jones", Email: "daniel.jones@example.com", Phone: "+1122334455"},
-		// Add more contacts as needed...
-	}
-	for _, contact := range contacts {
-		contactStore.AddContact(ctx, contact)
-	}
+
+	contactStore.SeedMe(ctx)
 
 	contactService := service.NewContactService(contactStore)
 
@@ -81,7 +66,7 @@ func run(ctx context.Context, logger *slog.Logger) error {
 		ContactService: contactService,
 	}
 	e := handler.NewMux(ctx, logger, services)
-	
+
 	// -------------------------------------------------------------------------
 	// App Starting
 
