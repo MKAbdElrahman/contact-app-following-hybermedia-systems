f8a430e43acec6e285cbf111cf241061c3a930f3
MKAbdElrahman <mkcodedev@gmail.com>
2023-12-16 15:47:26 +0200

    automatic push of url to the browser url

--- view/search_contact_templ.go
+++ view/search_contact_templ.go
@@ -47,7 +47,7 @@ func SearchContactsPageBody(c context.Context, data SearchContactsPageData) temp
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</label> <input hx-get=\"/contacts\" hx-target=\"#search-results\" hx-trigger=\"search, keyup delay:200ms changed\" id=\"search\" name=\"q\" type=\"search\" placeholder=\"Search Contacts\">")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</label> <input hx-get=\"/contacts\" hx-target=\"#search-results\" hx-trigger=\"search, keyup delay:200ms changed\" hx-push-url=\"true\" id=\"search\" name=\"q\" type=\"search\" placeholder=\"Search Contacts\">")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
